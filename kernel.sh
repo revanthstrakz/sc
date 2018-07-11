4f5e2f9e5642aef13e5ec00a005c556f5f13e98c
Revanth Strakz <dr.revanthstrakz@gmail.com>
2018-07-11 13:36:26 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -88,12 +88,12 @@ fi
 # Relatable
 colorize "${CYAN}"
 
-export CLANG_PATH=/home/adesikha15/clang/clang-7.0.2/bin
+export CLANG_PATH=~/clang/clang-r328903/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export TCHAIN_PATH="/home/adesikha15/gcc-4.9/bin/aarch64-linux-android-"
+export TCHAIN_PATH="~/gcc-4.9/bin/aarch64-linux-android-"
 export CROSS_COMPILE="${CCACHE} ${TCHAIN_PATH}"
-export CLANG_TCHAIN="/home/adesikha15/clang/clang-7.0.2/bin/clang"
+export CLANG_TCHAIN="~/clang/clang-r328903/bin/clang"
 export KBUILD_COMPILER_STRING="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 
 
